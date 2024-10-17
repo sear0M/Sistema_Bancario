0036db630e8fe2f8b9bcf38281eeb959e8af1756
searoM <pedro.moraes23032005@gmail.com>
2024-10-17 18:30:38 -0300

    Ajustes e Adicoes
    adicionado a funcao de buscar o usuario por saldo, feita a API da mesma e um ajuste nas outras APIs que estavam utilizando varias rotas com o mesmo prefixo mas parametros diferentes oq ocasionava em erros.

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ def root():
 def listar_usuarios():
     return funcoes.carregar_json()["usuarios"]
 
-@app.get("/usuarios/{id}")
+@app.get("/usuarios/id/{id}")
 def usuarios_id(id: int):
     return funcoes.buscar_por_id(id)
 
-@app.get("/usuarios/{nome}")
+@app.get("/usuarios/nome/{nome}")
 def usuarios_nomes(nome: str):
     return funcoes.buscar_nome(nome)
+
+@app.get("/usuarios/dinheiro/{dinheiro}")
+def usuarios_dinheiro(dinheiro: float):
+    return funcoes.buscar_por_saldo(dinheiro)
